c55aaf52345457e36c1a366a4c1ba528a18a1bba
nmermigas <nikosmermigas01@gmail.com>
2023-07-25 18:08:57 +0300

    adding hbs for template engine pages

---
+++ express-project/models/friends.model.js
@@ -0,0 +1,12 @@
+const friends = [
+  {
+    id: 0,
+    name: "John",
+  },
+  {
+    id: 1,
+    name: "Josh",
+  },
+];
+
+module.exports = friends;
--- express-project/server.js
+++ express-project/server.js
@@ -1,59 +1,34 @@
 const express = require("express");
+const path = require("path");
+
+const friendsRouter = require("./routes/friends.router");
+const messagesRouter = require("./routes/messages.router");
 
 const app = express();
 
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 const PORT = 3000;
-
-const friends = [
-  {
-    id: 0,
-    name: "John",
-  },
-  {
-    id: 1,
-    name: "Josh",
-  },
-];
 
 app.use((req, res, next) => {
   const start = Date.now();
   next();
   // actions go here...
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}`);
 });
 
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-app.post("/friends", (req, res) => {
-  if (!req.body.name) {
-    return res.status(400).json({ error: "Missing friend name" });
-  }
-  const newFriend = {
-    name: req.body.name,
-    id: friends.length,
-  };
-  friends.push(newFriend);
-
-  res.json(newFriend);
-});
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.render("index", {
+    title: "A Nice Dynamic Page Using HBS",
+    caption: "HIIIIIIII",
+  });
 });
-
-app.get("/friends", (req, res) => {
-  res.json(friends);
-});
-
-app.get("/friends/:friendId", (req, res) => {
-  const friendId = Number(req.params.friendId);
-  const friend = friends[friendId];
-  if (friend) {
-    res.status(200).json(friend);
-  } else {
-    res.status(404).json({ error: "Friend does not exist" });
-  }
-});
+app.use("/friends", friendsRouter);
+app.use("/messages", messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
