645b8316f23f329c75d96748524f0c08084f325d
nmermigas <nikosmermigas01@gmail.com>
2023-08-03 00:56:53 +0300

    fixing server

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -5,13 +5,14 @@ const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
-app.use(cors({
-    origin:"http://localhost"
-})); //middleware for cors: cross origin requests
+app.use(
+  cors({
+    origin: "http://localhost",
+  })
+); //middleware for cors: cross origin requests
 app.use(express.json());
 app.use(planetsRouter);
 
 module.exports = app;
-
 
 // convenient as it separates middleware from server
