e0aa24ab351fadd7c8e2c8dea613ce89edad23b7
nmermigkas <ge89vir@mytum.de>
2023-08-04 20:56:27 +0300

    updating package.json so that it can run on a single port, serving the public folder

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const cors = require("cors");
 
@@ -11,8 +12,13 @@ app.use(
   })
 ); //middleware for cors: cross origin requests
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'..','public')))
 app.use(planetsRouter);
 
+app.get('/',(req,res) => {
+  res.sendFile(path.join(__dirname,'..','public','index.html'))
+})
 module.exports = app;
 
 // convenient as it separates middleware from server
