254261270e562c47c1018882243080368767af6f
nmermigkas <ge89vir@mytum.de>
2023-08-04 19:14:15 +0300

    get planets works

--- nasa-project/server/src/routes/planets/planets.controller.js
+++ nasa-project/server/src/routes/planets/planets.controller.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const planets = require("../../models/planets.model");
-
-function getAllPlanets(req,res) {
-    return res.status(200).json(planets); //common practice since it stops executing after returning the status code.
-}
-
-module.exports = {
-    getAllPlanets,
-};
-=======
 const { planets } = require("../../models/planets.model");
 
 function getAllPlanets(req, res) {
@@ -18,4 +7,3 @@ function getAllPlanets(req, res) {
 module.exports = {
   getAllPlanets,
 };
->>>>>>> c3d12c47129c2c1edf2bd9105eed9cae6528efa6
