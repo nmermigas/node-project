2e17f9fe00f77406992bd78cfa8f65e5db13ff38
nmermigkas <ge89vir@mytum.de>
2023-08-04 22:41:17 +0300

    adding launches endpoint

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
-
+const launchesRouter = require("./routes/launches/launches.router");
 const app = express();
 
 app.use(
@@ -11,11 +12,13 @@ app.use(
     origin: "http://localhost:3000",
   })
 ); //middleware for cors: cross origin requests
+app.use(morgan('combined'))
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname,'..','public')))
 app.use(planetsRouter);
-
+app.use(launchesRouter);
 app.get('/',(req,res) => {
   res.sendFile(path.join(__dirname,'..','public','index.html'))
 })
