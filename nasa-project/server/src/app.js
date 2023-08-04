ae30f1e376c273c4f830221ee248361955709821
nmermigkas <ge89vir@mytum.de>
2023-08-04 19:14:15 +0300

    get planets works

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -5,24 +5,14 @@ const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
 
-<<<<<<< HEAD
-app.use(cors({
-    origin:"http://localhost"
-})); //middleware for cors: cross origin requests
-=======
 app.use(
   cors({
-    origin: "http://localhost",
+    origin: "http://localhost:3000",
   })
 ); //middleware for cors: cross origin requests
->>>>>>> c3d12c47129c2c1edf2bd9105eed9cae6528efa6
 app.use(express.json());
 app.use(planetsRouter);
 
 module.exports = app;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> c3d12c47129c2c1edf2bd9105eed9cae6528efa6
 // convenient as it separates middleware from server
