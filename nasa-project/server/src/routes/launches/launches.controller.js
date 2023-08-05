0a035e729bc50dbf21601db8a539c781a38bf348
nmermigkas <ge89vir@mytum.de>
2023-08-05 15:45:09 +0300

    adding launches route

--- nasa-project/server/src/routes/launches/launches.controller.js
+++ nasa-project/server/src/routes/launches/launches.controller.js
@@ -1,5 +1,10 @@
-const {launches} = require('./../models/launches.model')
+const {launches} = require('../../models/launches.model')
 
 function getAllLaunches(req, res) {
     return res.status(200).json(Array.from(launches.values()));
 }
+
+module.exports = {
+    getAllLaunches,
+  };
+  
