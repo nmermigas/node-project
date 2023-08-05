337c0507c1034dc1b1057266f5883d21abeadc69
nmermigkas <ge89vir@mytum.de>
2023-08-05 19:25:35 +0300

    adding POST launches

--- nasa-project/server/src/routes/planets/planets.controller.js
+++ nasa-project/server/src/routes/planets/planets.controller.js
@@ -1,9 +1,9 @@
-const { planets } = require("../../models/planets.model");
+const { getAllPlanets } = require("../../models/planets.model");
 
-function getAllPlanets(req, res) {
-  return res.status(200).json(planets); //common practice since it stops executing after returning the status code.
+function httpGetAllPlanets(req, res) {
+  return res.status(200).json(getAllPlanets()); //common practice since it stops executing after returning the status code.
 }
 
 module.exports = {
-  getAllPlanets,
+  httpGetAllPlanets,
 };
