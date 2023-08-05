7da20b77577101430f5aa9500e923b7d2ae639c4
nmermigkas <ge89vir@mytum.de>
2023-08-05 21:50:49 +0300

    adding more POST launches

--- nasa-project/server/src/routes/launches/launches.controller.js
+++ nasa-project/server/src/routes/launches/launches.controller.js
@@ -1,7 +1,14 @@
-const {getAllLaunches} = require('../../models/launches.model')
+const {getAllLaunches, addNewLaunch} = require('../../models/launches.model')
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches);
+}
+
+function httpAddLaunches(req,res) {
+    const launch = req.body;
+
+    launch.launcDate = new Date(launch.launchDate);
+    addNewLaunch(launch); 
 }
 
 module.exports = {
