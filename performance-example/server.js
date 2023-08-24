574c9f47b1dbc85f7bea0ce3154af7aa25523f2a
nmermigas <nikosmermigas01@gmail.com>
2023-08-24 17:32:39 +0300

    adding zero downtime restart with pm2

--- performance-example/server.js
+++ performance-example/server.js
@@ -1,31 +1,29 @@
-const express = require('express');
-
+const express = require("express");
 
 //The workers run the same code as the server.js
 
 const app = express();
 
-function delay(duration){
-    const startTime = Date.now();
+function delay(duration) {
+  const startTime = Date.now();
 
-    while (Date.now() - startTime < duration) {
-        //event loop is blocked
-    }
+  while (Date.now() - startTime < duration) {
+    //event loop is blocked
+  }
 }
 
-app.get('/', (req, res) => {
-    // JSON.stringify({}) => '{}'
-    // JSON.parse("{}") => {}
-    res.send(`Performance example ${process.pid}`);
-})
+app.get("/", (req, res) => {
+  // JSON.stringify({}) => '{}'
+  // JSON.parse("{}") => {}
+  res.send(`Performance example ${process.pid}`);
+});
 
-app.get('/timer', (req,res) =>{
-    delay(9000);
-    res.send(`Ding ding ding ${process.pid}`);
-})
+app.get("/timer", (req, res) => {
+  delay(4000);
+  res.send(`Beep beep beep ${process.pid}`);
+});
 
-console.log('Running Server.js...');
+console.log("Running Server.js...");
 //only as the master process is started
 console.log("Worker process has been started");
 app.listen(3001);
-
