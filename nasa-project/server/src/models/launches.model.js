bb580770ea0c84d8550c6d16ceca782126571f80
nmermigas <nikosmermigas01@gmail.com>
2023-09-06 18:55:36 +0300

    saving the launches from speceX api to mongo database

--- nasa-project/server/src/models/launches.model.js
+++ nasa-project/server/src/models/launches.model.js
@@ -44,6 +44,10 @@ async function populateLaunches() {
     },
   });
 
+  if (response.status != 200) {
+    console.log("Problem downloading launch data");
+    throw new Error("Launch data download failed");
+  }
   const launchDocs = response.data.docs;
   for (const launchDoc of launchDocs) {
     const payloads = launchDoc.payloads;
@@ -60,7 +64,7 @@ async function populateLaunches() {
     };
     console.log(`${launch.flightNumber} ${launch.mission}`);
 
-    // TODO: populate launches collection
+    await saveLaunch(launch);
   }
 }
 
@@ -99,14 +103,6 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({
-    keplerName: launch.target,
-  });
-
-  if (!planet) {
-    throw new Error("No matching planet found");
-  }
-
   await launchesDatabase.findOneAndUpdate(
     // it will only return the properties we set
     {
@@ -121,6 +117,13 @@ async function saveLaunch(launch) {
 
 // function to add some default properties when creating a new launch
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error("No matching planet found");
+  }
   const newFlightNumber = (await getLatestFlightNumber()) + 1;
   const newLaunch = Object.assign(launch, {
     success: true,
