8e27940136a7c9f30c87cacc74839d21479cb226
nmermigas <nikosmermigas01@gmail.com>
2023-09-23 21:17:08 +0300

    adding google oath2

--- security-example/server.js
+++ security-example/server.js
@@ -3,16 +3,76 @@ const path = require("path");
 const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
+
+require("dotenv").config();
 
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/auth",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+// checks if the access token and refresh token are valid
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile", profile);
+  done(null, profile);
+}
+
+//setting the passport strategy
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
 const app = express();
 
 // security related middleware usually goes at the top
 app.use(helmet());
 
-app.get("/secret", (req, res) => {
+app.use(passport.initialize());
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true; //TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({ error: "Must log in!" });
+  }
+  next();
+}
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"], //the data that we need
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back!");
+  }
+);
+
+app.get("/auth/logout", (req, res) => {});
+
+// we pass in middleware when defining the route. (we can add how many we want and they will run sequently)
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret is 42!");
+});
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to log in!");
 });
 
 app.get("/", (req, res) => {
